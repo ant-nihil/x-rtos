9f7f45b7d2f81498cad06296a67f6b9ac3bbab6b
ant-nihil <antnihil@outlook.com>
2022-04-14 21:59:17 +0800

    add:add new essential folders and files for x-rtos

---
+++ examples/stm32/main.c
@@ -0,0 +1,6 @@
+#include "../../include/includes.h"
+
+int main(int argc, int* argv[])
+{
+    printf("hello,world!");
+}
---
+++ include/defines.h
@@ -0,0 +1,10 @@
+//OS级别的各种宏定义,基本变量别名
+
+//对于32位系统
+typedef unsigned 	   char 	uint8_t ;	/*无符号8位*/
+typedef unsigned short int 		uint16_t;	/*无符号16位*/
+typedef unsigned	   int 		uint32_t;	/*无符号32位*/
+
+typedef   signed	   char 	int8_t;	    /*有符号8位*/
+typedef   signed short int 		int16_t;    /*有符号16位*/
+typedef   signed	   int 		uint32_t;	/*有符号32位*/

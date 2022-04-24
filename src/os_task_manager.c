379c2dda92a0f6dae1db5df8ca87899866d4ee8d
ant-nihil <antnihil@outlook.com>
2022-04-25 00:08:33 +0800

    add:add specific contents for 'os_task_create'file,not full-scale

--- src/os_task_manager.c
+++ src/os_task_manager.c
@@ -8,7 +8,34 @@ void OS_TASK_IdleTask(void)    //空任务
 }
 void OS_TASK_Create(void(*Task)(void),int32_t *p_Stack,int8_t TaskID)    //任务创建
 {
-
+    if(TaskID<=OS_TASK_NUM)
+	{
+		*(p_Stack) = (int32_t)Task;				/*PC*/
+		*(--p_Stack)=(int32_t)13;				/*LR*/
+		*(--p_Stack)=(int32_t)12;				/*R12*/
+		*(--p_Stack)=(int32_t)11;				/*R11*/
+		*(--p_Stack)=(int32_t)10;				/*R10*/
+		*(--p_Stack)=(int32_t)9;				/*R9*/
+		*(--p_Stack)=(int32_t)8;				/*R8*/
+		*(--p_Stack)=(int32_t)7;				/*R7*/
+		*(--p_Stack)=(int32_t)6;				/*R6*/
+		*(--p_Stack)=(int32_t)5;				/*R5*/
+		*(--p_Stack)=(int32_t)4;				/*R4*/
+		*(--p_Stack)=(int32_t)3;				/*R3*/
+		*(--p_Stack)=(int32_t)2;				/*R2*/
+		*(--p_Stack)=(int32_t)1;				/*R1*/
+		*(--p_Stack)=(int32_t)0;				/*R0*/
+//		*(--p_Stack)=(int32_t)(SVCMODE|0X0);	/*PSR保存在任务栈中*/
+		TCB[TaskID].OSTCBStkPtr=(int32_t)p_Stack;/*将人工堆栈的栈顶保存到堆栈的数组中*/
+		
+		TCB[TaskID].OSTCBDly=0;					/*初始化任务延时时间参数为0*/
+		OSSetPrioRdy(TaskID);					/*在任务就绪表中登记*/
+	}
+	else
+	{
+		//printf("TaskID Error\r\n");
+		while(1);
+	}
 }
 
 

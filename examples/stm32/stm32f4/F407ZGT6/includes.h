fc23e6fc7fd948c71331d601016d737815ec42c2
Sushishu <90468138+lucky-sushishu@users.noreply.github.com>
2022-09-15 00:53:05 +0800

    modify: modified project for a based example for test os

---
+++ examples/stm32/stm32f4/F407ZGT6/includes.h
@@ -0,0 +1,16 @@
+#ifndef __INCLUDES_H
+#define __INCLUDES_H
+
+// standard library
+#include "stm32f4xx.h"
+
+// hal
+#include "led_stm32.h"
+
+// x-rtos
+#include "x_rtos.h"
+#include "os_task_manager.h"
+#include "defines.h"
+
+
+#endif  // __INCLUDES_H
--- examples/stm32/stm32f4/F407ZGT6/main.c
+++ examples/stm32/stm32f4/F407ZGT6/main.c
@@ -1,33 +1,11 @@
-#include "stm32f4xx.h"
+#include "includes.h"
 
-void LED_Init(void);
 
 
 int main(int argc, char* argv[])
 {
-	LED_Init();
 	while(1)
 	{
-		GPIO_ResetBits(GPIOF,GPIO_Pin_9);  //LED0对应引脚GPIOF.9拉低，亮  等同LED0=0;
-		GPIO_SetBits(GPIOF,GPIO_Pin_10);   //LED1对应引脚GPIOF.10拉高，灭 等同LED1=1;
+		
 	}
-}
-
-
-void LED_Init(void)
-{    	 
-  GPIO_InitTypeDef  GPIO_InitStructure;
-
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);//使能GPIOF时钟
-
-  //GPIOF9,F10初始化设置
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;//LED0和LED1对应IO口
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;//普通输出模式
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
-  GPIO_Init(GPIOF, &GPIO_InitStructure);//初始化GPIO
-	
-	GPIO_SetBits(GPIOF,GPIO_Pin_9 | GPIO_Pin_10);//GPIOF9,F10设置高，灯灭
-
 }
